0bbacd802f0d4c8e7bbb4946daef2080e69d1c02
João Heguedusch <ojoao6926@gmail.com>
2022-10-03 15:29:56 -0300

    Add files via upload

---
+++ Funcao_Soma.py
@@ -0,0 +1,12 @@
+
+def sum(num1, num2):
+    return num1 + num2
+    
+print("\tSoma de dois números\n")
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+print ("")
+print (f"\tO resultado da soma é {sum(num1, num2)}")
+
+input()
