9e423c9f5cced54fc17e24a7334673d1bf2ffc88
João Heguedusch <ojoao6926@gmail.com>
2022-10-08 22:03:33 -0300

    Add files via upload

---
+++ Analisando-Triangulo.py
@@ -0,0 +1,14 @@
+r1 = float(input('Primeiro Segmento: '))
+r2 = float(input('Segundo Segmento: '))
+r3 = float(input('Terceiro Segmento: '))
+
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima pode formar um triângulo ', end='')
+    if r1 == r2 and r2 == r3:
+        print('EQUILATERO!')
+    elif r1 != r2 != r3 != r1:
+        print('ESCALENO!')
+    else:
+        print('ISÓCELES!')
+else:
+    print('Os segmentos informados não podem formar um triângulo')
